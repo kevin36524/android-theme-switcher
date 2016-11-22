2635d20ad5928a4bd0364213ff02b1f13421074a
Kevin Patel <patelkev@yahoo-inc.com>
2016-11-22 09:39:36 -0800

    Adding the themes as color variant

--- app/src/main/java/com/codepath/android/themeswitcher/app/ThemeActivity.java
+++ app/src/main/java/com/codepath/android/themeswitcher/app/ThemeActivity.java
@@ -1,17 +1,29 @@
 package com.codepath.android.themeswitcher.app;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 
 public class ThemeActivity extends ActionBarActivity {
+
+    public static int currentPosition;
+
+    private Spinner spThemes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theme);
+        Utils.onActivityCreateSetTheme(this);
+        // AFTER SETTING THEME
+        setContentView(R.layout.activity_theme);
+        setupSpinnerItemSelection();
+
     }
 
 
@@ -36,4 +48,29 @@ public class ThemeActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void setupSpinnerItemSelection() {
+        spThemes = (Spinner) findViewById(R.id.spThemes);
+        spThemes.setSelection(ThemeApplication.currentPosition);
+        ThemeApplication.currentPosition = spThemes.getSelectedItemPosition();
+
+        spThemes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                if (ThemeApplication.currentPosition != position) {
+                    Utils.changeToTheme(ThemeActivity.this, position);
+                }
+                ThemeApplication.currentPosition = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
 }
---
+++ app/src/main/java/com/codepath/android/themeswitcher/app/ThemeApplication.java
@@ -0,0 +1,11 @@
+package com.codepath.android.themeswitcher.app;
+
+import android.app.Application;
+
+/**
+ * Created by patelkev on 11/16/16.
+ */
+
+public class ThemeApplication extends Application {
+    public static int currentPosition;
+}
